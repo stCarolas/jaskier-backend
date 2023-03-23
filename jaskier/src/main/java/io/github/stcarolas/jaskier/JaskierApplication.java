f5906cd79d23229318f6af15f636f318bb8c54d1
stCarolas <stcarolas@gmail.com>
2023-03-23 04:01:50 +0300

    draft

---
+++ jaskier/src/main/java/io/github/stcarolas/jaskier/JaskierApplication.java
@@ -0,0 +1,13 @@
+package io.github.stcarolas.jaskier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaskierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaskierApplication.class, args);
+	}
+
+}
